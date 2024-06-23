88241184c1f2bf4bb4a787a9b970a822c0e4e5fd
sushen123 <sushensame@gmail.com>
2024-06-23 15:48:05 +0545

    Some changes

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const path = require("path")
 const mainRouter = require('./routes/index')
 const cors = require('cors')
 
-
+app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1", mainRouter)
 
-app.get("/", (req,res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+app.get("*", (req,res) => {
+  
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
 })
 
