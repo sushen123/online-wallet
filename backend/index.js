07a9f8403f0d8ecdb42319276f93d8423b1a9f17
sushen123 <sushensame@gmail.com>
2024-06-23 20:15:36 +0545

    Adding giles

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,11 @@ const mainRouter = require('./routes/index')
 const cors = require('cors')
 
 
-app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1", mainRouter)
 
-
-app.get("*", (req,res) => {
-    console.log(path.resolve(__dirname, 'frontend', 'dist'));
-    console.log(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-})
 
 app.listen(3000, () => {
     console.log("Port is running")
