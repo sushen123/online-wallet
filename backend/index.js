ce2013d6d44b6d504774a5090a4ab8364715470a
sushen123 <sushensame@gmail.com>
2024-06-23 15:21:17 +0545

    Adding frontend in backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require("path")
 const mainRouter = require('./routes/index')
 const cors = require('cors')
 
@@ -7,6 +8,11 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1", mainRouter)
+
+app.get("/", (req,res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.listen(3000, () => {
     console.log("Port is running")
