818d47477cb6de65d538861ee53ea56746854f71
sushen123 <sushensame@gmail.com>
2024-06-23 16:09:01 +0545

    vercel.jsos

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,10 @@ app.use(express.json())
 app.use(cors())
 app.use("/api/v1", mainRouter)
 
+
 app.get("*", (req,res) => {
-  
+    console.log(path.resolve(__dirname, 'frontend', 'dist'));
+    console.log(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
 })
 
